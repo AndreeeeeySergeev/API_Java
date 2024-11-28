ab3b0c5ebca048b611d222c5aeb0e33c47200e65
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2024-11-28 16:09:06 +0300

    Trying to create Weathers API

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static java.lang.String.format;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String x_Yandex_Weather_Key = "0cb02a21-c8dc-42d1-869a-529e9984ef10";
+        int lat = (int) 53.2521;
+        int lon = (int) 34.3717;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(format("https://api.weather.yandex.ru/v2/forecast?%.75&lon=37.62")))
+                .GET()
+                .header(x_Yandex_Weather_Key ,"application/json")
+                .build();
+
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Response Code: " + response.statusCode());
+            System.out.println("Response Body: " + response.body());
+        } catch (Exception e) {
+            System.err.println("Error making HTTP request: " + e.getMessage());
         }
+
     }
 }
